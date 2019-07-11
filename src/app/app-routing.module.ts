f82726e9bff818e218c211e88fdbf7261a753340
Garry Lachman <garry@lachman.co>
2019-07-11 18:30:08 +0300

    default redirect to accounts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        pathMatch: 'full',
+        redirectTo: '/accounts'
     },
     {
         path: 'accounts',
