2db0379a1d2261334edd189698a4e6774f665332
Garry Lachman <garry@lachman.co>
2023-01-12 01:37:58 +0200

    details tab scrolling

--- src/renderer/views/admin/views/data/details/data-details-card.tsx
+++ src/renderer/views/admin/views/data/details/data-details-card.tsx
@@ -11,7 +11,7 @@ type DataDetailsCardProperties = {
 export const DataDetailsCard: FC<DataDetailsCardProperties> = ({ data }) => {
   return (
     <Box p={0} m={0} flex={1} display="flex" flexDirection="column" h="100%">
-      <CardBody px={0} pb={0}>
+      <CardBody px={0} pb={0} overflow="scroll" py={0}>
         <DesignerDetails data={data} readOnly />
       </CardBody>
     </Box>
