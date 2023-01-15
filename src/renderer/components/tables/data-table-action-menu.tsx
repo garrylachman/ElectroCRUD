3c7427554b03c6900fb56690ff892292f27c3758
Garry Lachman <garry@lachman.co>
2023-01-16 01:34:42 +0200

    dropdown glass background

--- src/renderer/components/tables/data-table-action-menu.tsx
+++ src/renderer/components/tables/data-table-action-menu.tsx
@@ -32,7 +32,14 @@ export const DataTableActionMenu: FC<DataTableActionMenuProperties> = ({
       <Icon as={MdOutlineMoreVert} boxSize={6} mr={3} />
     </MenuButton>
     <Portal>
-      <MenuList rounded="lg" boxShadow="lg" py={0} overflow="hidden">
+      <MenuList
+        rounded="lg"
+        boxShadow="lg"
+        py={0}
+        overflow="hidden"
+        bgColor="whiteAlpha.500"
+        backdropFilter="blur(10px) contrast(100%) saturate(190%)"
+      >
         {items.map((item) => (
           <MenuItem
             key={item.label}
@@ -53,6 +60,7 @@ export const DataTableActionMenu: FC<DataTableActionMenuProperties> = ({
             iconSpacing={0}
             _hover={{ fontWeight: 'bold', bgColor: 'blackAlpha.200' }}
             icon={<Icon display="flex" as={item.menuIcon} boxSize={4} />}
+            bgColor="transparent"
           >
             {item.label}
           </MenuItem>
