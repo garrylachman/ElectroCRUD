9bb0f18f9c5152dc2b5f08c3958562d845301278
Garry Lachman <garry@lachman.co>
2023-02-13 16:20:41 +0200

    buttons package test

--- packages/buttons/__tests__/buttons.test.tsx
+++ packages/buttons/__tests__/buttons.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { RippleButton } from '../src/ripple-button';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../../../src/renderer/theme/theme';
 
-test('renders with text', () => {
-  const { getByText } = render(
+test('button with text', () => {
+  const { getByText, getByRole } = render(
     <ChakraProvider theme={theme}>
       <RippleButton variant="solid" bgColorScheme="primary" size="lg">
         Save
@@ -13,4 +13,43 @@ test('renders with text', () => {
     </ChakraProvider>
   );
   expect(getByText('Save')).toBeTruthy();
+  expect(getByRole('button')).not.toBeDisabled();
+});
+
+test('disabled button', () => {
+  const { getByRole } = render(
+    <ChakraProvider theme={theme}>
+      <RippleButton isDisabled>Save</RippleButton>
+    </ChakraProvider>
+  );
+  expect(getByRole('button')).toBeDisabled();
+  expect(getByRole('button')).toHaveAttribute('disabled');
+});
+
+test('click', () => {
+  const clickFunction = jest.fn();
+  const { getByRole } = render(
+    <ChakraProvider theme={theme}>
+      <RippleButton onClick={clickFunction}>Save</RippleButton>
+    </ChakraProvider>
+  );
+
+  const button = getByRole('button');
+  fireEvent.click(button);
+  expect(clickFunction).toHaveBeenCalledTimes(1);
+});
+
+test('click disabled', () => {
+  const clickFunction = jest.fn();
+  const { getByRole } = render(
+    <ChakraProvider theme={theme}>
+      <RippleButton onClick={clickFunction} isDisabled>
+        Save
+      </RippleButton>
+    </ChakraProvider>
+  );
+
+  const button = getByRole('button');
+  fireEvent.click(button);
+  expect(clickFunction).toHaveBeenCalledTimes(0);
 });
