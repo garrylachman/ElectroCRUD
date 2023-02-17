d9ea8c5d552812832c13ff0c1fd4f14aaa53b914
Garry Lachman <garry@lachman.co>
2023-02-17 15:12:09 +0200

    Views > view - moved to layouts

--- src/renderer/views/admin/views/data/documentation/index.tsx
+++ src/renderer/views/admin/views/data/documentation/index.tsx
@@ -1,6 +1,5 @@
-import { Card, CardBody } from '@chakra-ui/card';
 import { useContext } from 'react';
-import { Divider } from '@chakra-ui/react';
+import { Divider, Flex } from '@chakra-ui/react';
 import { ViewScopedContext } from 'renderer/contexts';
 import { TableDocumentation } from './table-documentation';
 import { ColumnDocumentation } from './column-documentation';
@@ -10,26 +9,14 @@ export const Documentation = () => {
   const { viewState } = useContext(ViewScopedContext);
 
   return (
-    <Card
-      variant="elevated"
-      flex={1}
-      display="flex"
-      flexDirection="column"
-      height="100%"
-      overscrollBehavior="contain"
-      overflow="scroll"
-    >
-      <CardBody>
-        <TableDocumentation />
-        {viewState?.columns.map((row) => (
-          <>
-            <Divider my={5} />
-            <ColumnDocumentation
-              columnState={row as unknown as StrictColumnRO}
-            />
-          </>
-        ))}
-      </CardBody>
-    </Card>
+    <Flex flexDirection="column">
+      <TableDocumentation />
+      {viewState?.columns.map((row) => (
+        <>
+          <Divider my={5} />
+          <ColumnDocumentation columnState={row as unknown as StrictColumnRO} />
+        </>
+      ))}
+    </Flex>
   );
 };
