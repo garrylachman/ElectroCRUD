da54747b9a2f5495281fbe8c4b261456b3c290da
Garry Lachman <garry@lachman.co>
2023-06-05 18:22:50 +0300

    cleanup

--- src/shared/ipc/views.ipc.ts
+++
@@ -1,7 +0,0 @@
-export * from './views/tables-list.ipc';
-export * from './views/table-info.ipc';
-export * from './views/read-data.ipc';
-export * from './views/insert-data.ipc';
-export * from './views/update-data.ipc';
-export * from './views/delete-data.ipc';
-export * from './views/read-widget-data.ipc';
